abf78e20211ad782b477ac21ea9d6ca28ba945c9
Adrian <adrianfernandezj@gmail.com>
2021-01-12 14:27:11 -0300

    firste commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import './App.js';
+import Header from './components/Header';
+import Main from './components/Main';
+import SectionOne from './components/SectionOne';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <SectionOne />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SectionOne.js
@@ -0,0 +1,250 @@
+import React from 'react';
+
+const sectionOne = () => {
+    return ( 
+        <div className="bg-gray-200 container max-w-full mx-auto py-24 px-6">
+            <h1
+            className="text-center text-4xl text-gray-800 font-bold leading-snug"
+            >
+            Precio
+            </h1>
+            <p className="text-center text-lg text-gray-700 mt-2 px-6">
+                Te ofrecemos distintos planes para que puedas elegir el mejor que se adapte a tu negocio.
+            </p>
+
+            <div className="bg-yellow-200 max-w-full md:max-w-6xl mx-auto my-3 md:px-8">
+                <div
+                    className="relative block flex flex-col md:flex-row items-center"
+                ></div>
+                <div
+                    className="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4"
+                ></div>
+                <div
+                className="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
+                ></div>
+                <div
+                className="bg-white block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
+                >
+                <h1
+                className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
+                >
+                Básico 
+                </h1>
+                <h2 className="text-sm text-gray-500 text-center pb-6">$0.000/mes</h2>
+                Stripe offers everything needed to run an online business
+                at scale. Get in touch for details.
+                
+                <div className="flex flex-wrap mt-3 px-6">
+	<ul>
+		<li className="flex items-center">
+			<div
+			className=" rounded-full p-2 fill-current text-green-700"
+			>
+			<svg
+			className="w-6 h-6 align-middle"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			>
+			<path
+			d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+			></path>
+			<polyline
+			points="22 4 12 14.01 9 11.01"
+			></polyline>
+		</svg>
+	</div>
+	<span className="text-gray-700 text-lg ml-3"
+	>No setup</span
+	>
+</li>
+<li className="flex items-center">
+	<div
+	className=" rounded-full p-2 fill-current text-green-700"
+	>
+	<svg
+	className="w-6 h-6 align-middle"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	>
+	<path
+	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+	></path>
+	<polyline
+	points="22 4 12 14.01 9 11.01"
+	></polyline>
+</svg>
+</div>
+<span className="text-gray-700 text-lg ml-3"
+>No setups</span
+>
+</li>
+<li className="flex items-center">
+	<div
+	className=" rounded-full p-2 fill-current text-green-700"
+	>
+	<svg
+	className="w-6 h-6 align-middle"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	>
+	<path
+	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+	></path>
+	<polyline
+	points="22 4 12 14.01 9 11.01"
+	></polyline>
+</svg>
+</div>
+<span className="text-gray-700 text-lg ml-3">Speed</span>
+</li>
+</ul> 
+</div>
+<div className="block flex items-center p-8  uppercase">
+	<button
+	className="mt-3 text-lg font-semibold 
+	bg-gray-800 w-full text-white rounded-lg 
+	px-6 py-3 block hover:bg-gray-700"
+	>
+	Elegir
+</button>
+</div>
+
+                </div>
+
+
+                <div
+                className="bg-white block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
+                >
+                <h1
+                className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
+                >
+                Básico 
+                </h1>
+                <h2 className="text-sm text-gray-500 text-center pb-6">usd 17/mes</h2>
+                Todo el Servicio de gestion y Administracion de Turno
+                Direccion Web en Easy Turno
+                Servicio Tecnico 24/7 Web easy.
+                Boton de Whatsaap en Pagina Principal .(Opcional)
+                Plantilla Estandar + Logo Empresa.
+                
+                <div className="flex flex-wrap mt-3 px-6">
+	<ul>
+		<li className="flex items-center">
+			<div
+			className=" rounded-full p-2 fill-current text-green-700"
+			>
+			<svg
+			className="w-6 h-6 align-middle"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			>
+			<path
+			d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+			></path>
+			<polyline
+			points="22 4 12 14.01 9 11.01"
+			></polyline>
+		</svg>
+	</div>
+	<span className="text-gray-700 text-lg ml-3"
+	>No setup</span
+	>
+</li>
+<li className="flex items-center">
+	<div
+	className=" rounded-full p-2 fill-current text-green-700"
+	>
+	<svg
+	className="w-6 h-6 align-middle"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	>
+	<path
+	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+	></path>
+	<polyline
+	points="22 4 12 14.01 9 11.01"
+	></polyline>
+</svg>
+</div>
+<span className="text-gray-700 text-lg ml-3"
+>No setups</span
+>
+</li>
+<li className="flex items-center">
+	<div
+	className=" rounded-full p-2 fill-current text-green-700"
+	>
+	<svg
+	className="w-6 h-6 align-middle"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	>
+	<path
+	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+	></path>
+	<polyline
+	points="22 4 12 14.01 9 11.01"
+	></polyline>
+</svg>
+</div>
+<span className="text-gray-700 text-lg ml-3">Speed</span>
+</li>
+</ul> 
+</div>
+<div className="block flex items-center p-8  uppercase">
+	<button
+	className="mt-3 text-lg font-semibold 
+	bg-gray-800 w-full text-white rounded-lg 
+	px-6 py-3 block hover:bg-gray-700"
+	>
+	Elegir
+</button>
+</div>
+
+                </div>
+                
+                
+            </div>
+            
+	    </div>
+     );
+}
+ 
+export default sectionOne;
