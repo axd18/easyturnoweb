c488bb472dd3812212abe30edbd463245e8b73ac
adrian <adrianfernandezj@gmail.com>
2021-01-12 21:23:58 -0300

    Sección de precios

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -2,39 +2,50 @@ import React from 'react';
 
 const sectionOne = () => {
     return ( 
-        <div className="bg-gray-200 container max-w-full mx-auto py-24 px-6">
-            <h1
-            className="text-center text-4xl text-gray-800 font-bold leading-snug"
-            >
-            Precio
-            </h1>
-            <p className="text-center text-lg text-gray-700 mt-2 px-6">
-                Te ofrecemos distintos planes para que puedas elegir el mejor que se adapte a tu negocio.
-            </p>
+        
+<section>
+	<div className="bg-gray-200 container max-w-full mx-auto py-24 px-6">
+		<h1
+		className="text-center text-4xl text-black font-medium leading-snug"
+		>
+		Precio
+	</h1>
+	<p className="text-center text-lg text-gray-700 mt-2 px-6">
+		Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+		accusantium doloremque laudantium, totam rem aperiam.
+	</p>
+	<div
+	className="h-1 mx-auto bg-indigo-200 w-24 opacity-75 mt-4 rounded"
+	></div>
 
-            <div className="bg-yellow-200 max-w-full md:max-w-6xl mx-auto my-3 md:px-8">
-                <div
-                    className="relative block flex flex-col md:flex-row items-center"
-                ></div>
-                <div
-                    className="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4"
-                ></div>
-                <div
-                className="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
-                ></div>
-                <div
-                className="bg-white block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
-                >
-                <h1
-                className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
-                >
-                Básico 
-                </h1>
-                <h2 className="text-sm text-gray-500 text-center pb-6">$0.000/mes</h2>
-                Stripe offers everything needed to run an online business
-                at scale. Get in touch for details.
-                
-                <div className="flex flex-wrap mt-3 px-6">
+	<div className="max-w-full md:max-w-6xl mx-auto my-3 md:px-8">
+		<div
+		className="relative block flex flex-col md:flex-row items-center"
+		>
+		<div
+		className="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4"
+		>
+		<div
+		className="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
+		>
+		<div
+		className="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
+		>
+		<h1
+		className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
+		>
+		Plan Básico
+	</h1>
+	<h2 className="text-sm text-gray-500 text-center pb-6">usd 17/mes</h2>
+	
+	Todo el Servicio de gestion y Administracion de Turno
+	Direccion Web en Easy Turno
+	Servicio Tecnico 24/7 Web easy.
+	Boton de Whatsaap en Pagina Principal .(Opcional)
+	Plantilla Estandar + Logo Empresa.
+</div>
+
+<div className="flex flex-wrap mt-3 px-6">
 	<ul>
 		<li className="flex items-center">
 			<div
@@ -60,7 +71,7 @@ const sectionOne = () => {
 		</svg>
 	</div>
 	<span className="text-gray-700 text-lg ml-3"
-	>No setup</span
+	>Servicio Tecnico 24/7</span
 	>
 </li>
 <li className="flex items-center">
@@ -87,7 +98,7 @@ const sectionOne = () => {
 </svg>
 </div>
 <span className="text-gray-700 text-lg ml-3"
->No setups</span
+>Botón de Whatsapp</span
 >
 </li>
 <li className="flex items-center">
@@ -113,39 +124,42 @@ const sectionOne = () => {
 	></polyline>
 </svg>
 </div>
-<span className="text-gray-700 text-lg ml-3">Speed</span>
+<span className="text-gray-700 text-lg ml-3">Plantilla</span>
 </li>
 </ul> 
-</div>
+</div>  
 <div className="block flex items-center p-8  uppercase">
 	<button
 	className="mt-3 text-lg font-semibold 
-	bg-gray-800 w-full text-white rounded-lg 
-	px-6 py-3 block hover:bg-gray-700"
+	bg-black w-full text-white rounded-lg 
+	px-6 py-3 block shadow-xl hover:bg-gray-700"
 	>
-	Elegir
+	Select
 </button>
 </div>
+</div>
+</div>
 
-                </div>
+<div
+className="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-ml-4"
+>
+<div
+className="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
+>
+<div
+className="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
+>
+<h1
+className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
+>
+Enteprise
+</h1>
+<h2 className="text-sm text-gray-500 text-center pb-6">€39 /mo</h2> 
 
-
-                <div
-                className="bg-white block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
-                >
-                <h1
-                className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
-                >
-                Básico 
-                </h1>
-                <h2 className="text-sm text-gray-500 text-center pb-6">usd 17/mes</h2>
-                Todo el Servicio de gestion y Administracion de Turno
-                Direccion Web en Easy Turno
-                Servicio Tecnico 24/7 Web easy.
-                Boton de Whatsaap en Pagina Principal .(Opcional)
-                Plantilla Estandar + Logo Empresa.
-                
-                <div className="flex flex-wrap mt-3 px-6">
+Stripe offers everything needed to run an online business
+at scale. Get in touch for details.
+</div>
+<div className="flex flex-wrap mt-3 px-6">
 	<ul>
 		<li className="flex items-center">
 			<div
@@ -171,7 +185,7 @@ const sectionOne = () => {
 		</svg>
 	</div>
 	<span className="text-gray-700 text-lg ml-3"
-	>No setup</span
+	>Electric</span
 	>
 </li>
 <li className="flex items-center">
@@ -198,7 +212,7 @@ const sectionOne = () => {
 </svg>
 </div>
 <span className="text-gray-700 text-lg ml-3"
->No setups</span
+>Monthly</span
 >
 </li>
 <li className="flex items-center">
@@ -224,26 +238,28 @@ const sectionOne = () => {
 	></polyline>
 </svg>
 </div>
-<span className="text-gray-700 text-lg ml-3">Speed</span>
+<span className="text-gray-700 text-lg ml-3"
+>No setup</span
+>
 </li>
-</ul> 
+</ul>
 </div>
+
 <div className="block flex items-center p-8  uppercase">
 	<button
 	className="mt-3 text-lg font-semibold 
-	bg-gray-800 w-full text-white rounded-lg 
-	px-6 py-3 block hover:bg-gray-700"
+	bg-black w-full text-white rounded-lg 
+	px-6 py-3 block shadow-xl hover:bg-gray-700"
 	>
-	Elegir
+	Select
 </button>
 </div>
-
-                </div>
-                
-                
-            </div>
-            
-	    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
      );
 }
  
