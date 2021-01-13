1c5128a98dbcfe5e79f5b83467a024d9f13f3615
Adrian <adrianfernandezj@gmail.com>
2021-01-13 12:46:14 -0300

    maquetando precios

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -6,261 +6,242 @@ const sectionOne = () => {
 <section>
 	<div className="bg-gray-200 container max-w-full mx-auto py-24 px-6">
 		<h1
-		className="text-center text-4xl text-black font-medium leading-snug"
-		>
-		Precio
-	</h1>
-	<p className="text-center text-lg text-gray-700 mt-2 px-6">
-		Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-		accusantium doloremque laudantium, totam rem aperiam.
-	</p>
-	<div
-	className="h-1 mx-auto bg-indigo-200 w-24 opacity-75 mt-4 rounded"
-	></div>
+			className="text-center text-4xl text-black font-medium leading-snug"
+			>
+			Precio
+		</h1>
+		<p className="text-center text-lg text-gray-700 mt-2 px-6">
+			Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+			accusantium doloremque laudantium, totam rem aperiam.
+		</p>
 
-	<div className="max-w-full md:max-w-6xl mx-auto my-3 md:px-8">
-		<div
-		className="relative block flex flex-col md:flex-row items-center"
-		>
-		<div
-		className="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-mr-4"
-		>
-		<div
-		className="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
-		>
-		<div
-		className="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
-		>
-		<h1
-		className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
-		>
-		Plan Básico
-	</h1>
-	<h2 className="text-sm text-gray-500 text-center pb-6">usd 17/mes</h2>
-	
-	Todo el Servicio de gestion y Administracion de Turno
-	Direccion Web en Easy Turno
-	Servicio Tecnico 24/7 Web easy.
-	Boton de Whatsaap en Pagina Principal .(Opcional)
-	Plantilla Estandar + Logo Empresa.
-</div>
 
-<div className="flex flex-wrap mt-3 px-6">
-	<ul>
-		<li className="flex items-center">
+		<div class="container mx-auto flex justify-center mt-10">
+
 			<div
-			className=" rounded-full p-2 fill-current text-green-700"
+				className=" bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden mx-4"
 			>
-			<svg
-			className="w-6 h-6 align-middle"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			>
-			<path
-			d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-			></path>
-			<polyline
-			points="22 4 12 14.01 9 11.01"
-			></polyline>
-		</svg>
-	</div>
-	<span className="text-gray-700 text-lg ml-3"
-	>Servicio Tecnico 24/7</span
-	>
-</li>
-<li className="flex items-center">
-	<div
-	className=" rounded-full p-2 fill-current text-green-700"
-	>
-	<svg
-	className="w-6 h-6 align-middle"
-	width="24"
-	height="24"
-	viewBox="0 0 24 24"
-	fill="none"
-	stroke="currentColor"
-	stroke-width="2"
-	stroke-linecap="round"
-	stroke-linejoin="round"
-	>
-	<path
-	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-	></path>
-	<polyline
-	points="22 4 12 14.01 9 11.01"
-	></polyline>
-</svg>
-</div>
-<span className="text-gray-700 text-lg ml-3"
->Botón de Whatsapp</span
->
-</li>
-<li className="flex items-center">
-	<div
-	className=" rounded-full p-2 fill-current text-green-700"
-	>
-	<svg
-	className="w-6 h-6 align-middle"
-	width="24"
-	height="24"
-	viewBox="0 0 24 24"
-	fill="none"
-	stroke="currentColor"
-	stroke-width="2"
-	stroke-linecap="round"
-	stroke-linejoin="round"
-	>
-	<path
-	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-	></path>
-	<polyline
-	points="22 4 12 14.01 9 11.01"
-	></polyline>
-</svg>
-</div>
-<span className="text-gray-700 text-lg ml-3">Plantilla</span>
-</li>
-</ul> 
-</div>  
-<div className="block flex items-center p-8  uppercase">
-	<button
-	className="mt-3 text-lg font-semibold 
-	bg-black w-full text-white rounded-lg 
-	px-6 py-3 block shadow-xl hover:bg-gray-700"
-	>
-	Select
-</button>
-</div>
-</div>
-</div>
+				<div
+					className="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
+				>
+					<h1
+					className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
+					>
+					Plan Básico
+					</h1>
+					<h2 className="text-sm text-gray-500 text-center pb-6">usd 17/mes</h2>
+						Todo el Servicio de gestion y Administracion de Turno
+						Direccion Web en Easy Turno
+						Servicio Tecnico 24/7 Web easy.
+						Boton de Whatsaap en Pagina Principal .(Opcional)
+						Plantilla Estandar + Logo Empresa.
+				</div>
 
-<div
-className="w-11/12 max-w-sm sm:w-3/5 lg:w-1/3 sm:my-5 my-8 relative z-0 rounded-lg shadow-lg md:-ml-4"
->
-<div
-className="bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden"
->
-<div
-className="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
->
-<h1
-className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
->
-Enteprise
-</h1>
-<h2 className="text-sm text-gray-500 text-center pb-6">€39 /mo</h2> 
+				<div className="flex flex-wrap mt-3 px-6">
+					<ul>
+						<li className="flex items-center">
+							<div
+								className=" rounded-full p-2 fill-current text-green-700"
+							>
+								<svg
+										className="w-6 h-6 align-middle"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									>
+									<path
+										d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+									></path>
+									<polyline
+										points="22 4 12 14.01 9 11.01"
+									></polyline>
+								</svg>
+							</div>
+							<span className="text-gray-700 text-lg ml-3">Servicio Tecnico 24/7</span>
+						</li>
+						<li className="flex items-center">
+							<div
+								className=" rounded-full p-2 fill-current text-green-700"
+								>
+								<svg
+									className="w-6 h-6 align-middle"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									>
+									<path
+										d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+									></path>
+									<polyline
+									points="22 4 12 14.01 9 11.01"
+									></polyline>
+								</svg>
+							</div>
+							<span className="text-gray-700 text-lg ml-3">Botón de Whatsapp</span>
+						</li>
+						<li className="flex items-center">
+							<div
+								className=" rounded-full p-2 fill-current text-green-700"
+							>
+								<svg
+									className="w-6 h-6 align-middle"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								>
+									<path
+									d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+									></path>
+									<polyline
+									points="22 4 12 14.01 9 11.01"
+									></polyline>
+								</svg>
+							</div>
+							<span className="text-gray-700 text-lg ml-3">Plantilla</span>
+						</li>
+					</ul> 
+				</div>  
+				<div className="block flex items-center p-8  uppercase">
+					<button
+						className="mt-3 text-lg font-semibold 
+						bg-black w-full text-white rounded-lg 
+						px-6 py-3 block shadow-xl hover:bg-gray-700"
+					>
+					Select
+					</button>
+				</div>
+			</div>
 
-Stripe offers everything needed to run an online business
-at scale. Get in touch for details.
-</div>
-<div className="flex flex-wrap mt-3 px-6">
-	<ul>
-		<li className="flex items-center">
 			<div
-			className=" rounded-full p-2 fill-current text-green-700"
+				className=" bg-white text-black rounded-lg shadow-inner shadow-lg overflow-hidden mx-4"
 			>
-			<svg
-			className="w-6 h-6 align-middle"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			>
-			<path
-			d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-			></path>
-			<polyline
-			points="22 4 12 14.01 9 11.01"
-			></polyline>
-		</svg>
-	</div>
-	<span className="text-gray-700 text-lg ml-3"
-	>Electric</span
-	>
-</li>
-<li className="flex items-center">
-	<div
-	className=" rounded-full p-2 fill-current text-green-700"
-	>
-	<svg
-	className="w-6 h-6 align-middle"
-	width="24"
-	height="24"
-	viewBox="0 0 24 24"
-	fill="none"
-	stroke="currentColor"
-	stroke-width="2"
-	stroke-linecap="round"
-	stroke-linejoin="round"
-	>
-	<path
-	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-	></path>
-	<polyline
-	points="22 4 12 14.01 9 11.01"
-	></polyline>
-</svg>
-</div>
-<span className="text-gray-700 text-lg ml-3"
->Monthly</span
->
-</li>
-<li className="flex items-center">
-	<div
-	className=" rounded-full p-2 fill-current text-green-700"
-	>
-	<svg
-	className="w-6 h-6 align-middle"
-	width="24"
-	height="24"
-	viewBox="0 0 24 24"
-	fill="none"
-	stroke="currentColor"
-	stroke-width="2"
-	stroke-linecap="round"
-	stroke-linejoin="round"
-	>
-	<path
-	d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
-	></path>
-	<polyline
-	points="22 4 12 14.01 9 11.01"
-	></polyline>
-</svg>
-</div>
-<span className="text-gray-700 text-lg ml-3"
->No setup</span
->
-</li>
-</ul>
-</div>
+				<div
+					className="block text-left text-sm sm:text-md max-w-sm mx-auto mt-2 text-black px-8 lg:px-6"
+				>
+					<h1
+					className="text-lg font-medium uppercase p-3 pb-0 text-center tracking-wide"
+					>
+					Plan Básico
+					</h1>
+					<h2 className="text-sm text-gray-500 text-center pb-6">usd 17/mes</h2>
+						Todo el Servicio de gestion y Administracion de Turno
+						Direccion Web en Easy Turno
+						Servicio Tecnico 24/7 Web easy.
+						Boton de Whatsaap en Pagina Principal .(Opcional)
+						Plantilla Estandar + Logo Empresa.
+				</div>
 
-<div className="block flex items-center p-8  uppercase">
-	<button
-	className="mt-3 text-lg font-semibold 
-	bg-black w-full text-white rounded-lg 
-	px-6 py-3 block shadow-xl hover:bg-gray-700"
-	>
-	Select
-</button>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+				<div className="flex flex-wrap mt-3 px-6">
+					<ul>
+						<li className="flex items-center">
+							<div
+								className=" rounded-full p-2 fill-current text-green-700"
+							>
+								<svg
+										className="w-6 h-6 align-middle"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="2"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									>
+									<path
+										d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+									></path>
+									<polyline
+										points="22 4 12 14.01 9 11.01"
+									></polyline>
+								</svg>
+							</div>
+							<span className="text-gray-700 text-lg ml-3">Servicio Tecnico 24/7</span>
+						</li>
+						<li className="flex items-center">
+							<div
+								className=" rounded-full p-2 fill-current text-green-700"
+								>
+								<svg
+									className="w-6 h-6 align-middle"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									>
+									<path
+										d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+									></path>
+									<polyline
+									points="22 4 12 14.01 9 11.01"
+									></polyline>
+								</svg>
+							</div>
+							<span className="text-gray-700 text-lg ml-3">Botón de Whatsapp</span>
+						</li>
+						<li className="flex items-center">
+							<div
+								className=" rounded-full p-2 fill-current text-green-700"
+							>
+								<svg
+									className="w-6 h-6 align-middle"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								>
+									<path
+									d="M22 11.08V12a10 10 0 1 1-5.93-9.14"
+									></path>
+									<polyline
+									points="22 4 12 14.01 9 11.01"
+									></polyline>
+								</svg>
+							</div>
+							<span className="text-gray-700 text-lg ml-3">Plantilla</span>
+						</li>
+					</ul> 
+				</div>  
+				<div className="block flex items-center p-8  uppercase">
+					<button
+						className="mt-3 text-lg font-semibold 
+						bg-black w-full text-white rounded-lg 
+						px-6 py-3 block shadow-xl hover:bg-gray-700"
+					>
+					Select
+					</button>
+				</div>
+			</div>
+
+		</div>
+		
+	</div>
 </section>
-     );
+	 
+	 );
 }
  
 export default sectionOne;
