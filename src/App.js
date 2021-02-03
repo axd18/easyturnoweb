cc4987e9e9f67cc44e25d3d1b3c84d8b957d6074
Adrian <adrianfernandezj@gmail.com>
2021-02-03 07:49:03 -0300

    trabajando con router

--- src/App.js
+++ src/App.js
@@ -10,21 +10,30 @@ import Footer from './components/Footer';
 import Arrow from './components/Arrow';
 import Suscrip from './components/Suscrip';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+
 function App() {
   
 return (
-    <>
-      <Navbar />
-      <Main id="section0"/>
-      <QueEs id="section1"/>
-      <ComoFunciona id="section2"/>
-      <Precios id="section3"/>
-      <Faq id="section4"/>
-      <Form id="section5"/>
-      <Suscrip />
-      <Footer />
-      <Arrow />
-    </>
+    <Fragment>
+      <Router>
+          <Navbar />
+        <Switch>
+          <Route exact path="/" component={Main} id="section0"/>
+          <Route exact path="/" component={QueEs} id="section1"/>
+          {/* <Main id="section0"/> */}
+          {/* <QueEs id="section1"/>
+          <ComoFunciona id="section2"/>
+          <Precios id="section3"/>
+          <Faq id="section4"/>
+          <Form id="section5"/>
+          <Suscrip />
+          <Footer />
+          <Arrow /> */}
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
