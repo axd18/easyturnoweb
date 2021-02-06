361a2a03c5e31a30b647ce4ffcdffa0568f59014
adrian <adrianfernandezj@gmail.com>
2021-02-06 17:56:35 -0300

    Browser Router

--- src/App.js
+++ src/App.js
@@ -10,28 +10,23 @@ import Footer from './components/Footer';
 import Arrow from './components/Arrow';
 import Suscrip from './components/Suscrip';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   
 return (
     <>
       <Router>
-          {/* <Navbar /> */}
-        
-          {/* <Route exact strict path="/" component={Main} id="section0"/>
+          <Route exact strict path="/" component={Navbar}/>
+          <Route exact strict path="/" component={Main} id="section0"/>
           <Route exact strict path="/" component={QueEs} id="section1"/>
-          <Route exact strict path="/" component={ComoFunciona} id="section1"/>
-          <Main id="section0"/> */
-          /* <QueEs id="section1"/>
-          <ComoFunciona id="section2"/>
-          <Precios id="section3"/>
-          <Faq id="section4"/>
-          <Form id="section5"/> */}
-          <Suscrip />
-          {/* <Arrow /> */}
-        
+          <Route exact strict path="/" component={ComoFunciona} id="section2"/>
+          <Route exact strict path="/" component={Precios} id="section3"/>
+          <Route exact strict path="/" component={Faq} id="section4"/>
+          <Route exact strict path="/" component={Form} id="section5"/>
+          <Route exact strict path="/" component={Footer}/>
+          <Route exact strict path="/" component={Arrow}/>
+          <Route exact strict path="/suscripcion" component={Suscrip}/>
       </Router>
     </>
   );
--- src/components/Suscrip.js
+++ src/components/Suscrip.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import suscri from '../assets/img/como.svg';
+import emailjs from 'emailjs-com';
 
 const Suscrip = () => {
-    return (
+    
+    const handleSubmit = e => {
+        e.preventDefault();
 
-    <div className=" container mx-auto content-center align-center bg-white flex items-center justify-center px-5 py-5">
+        emailjs.sendForm('service_igde7xi', 'template_yt66dnm', e.target, 'user_DjxbpiGhrI5bsn5NN7Lm7')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+    }
+    
+    return (
+    <div className="bg-gray-200 w-screen h-screen">
+    <div className="container mx-auto content-center align-center flex items-center justify-center w-screen h-screen">
         <div className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" >
             <div className="md:flex w-full">
                 <div className="hidden md:block w-1/2 bg-purple-700 py-10 px-10">
@@ -16,20 +29,20 @@ const Suscrip = () => {
                         <h1 className="font-bold text-3xl">¡Muchas Gracias por suscribirte!</h1>
                         <p>A continuación te pedimos algunos datos para poder configurar tu cuenta y que empieces a recibir los turnos.</p>
                     </div>
-                    <div>
+                    <form onSubmit={ handleSubmit }>
                         <div className="md:flex -mx-3">
                             <div className="md:w-1/2 px-3 mb-5">
                                 <label for="" className="text-xs font-semibold px-1">Nombre</label>
                                 <div className="flex">
                                     <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                    <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 text-gray-700" placeholder="Nombre"></input>
+                                    <input type="text" name="nombre" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-400 outline-none focus:border-indigo-500 text-gray-700" placeholder="Nombre"  onChange={handleSubmit}></input>
                                 </div>
                             </div>
                             <div className="md:w-1/2 px-3 mb-5">
                                 <label for="" className="text-xs font-semibold px-1">Nombre del negocio</label>
                                 <div className="flex">
                                     <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                    <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 text-gray-700" placeholder="Nombre del negocio"></input>
+                                    <input type="text" name="nombre_negocio" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-400 outline-none focus:border-indigo-500 text-gray-700" placeholder="Nombre del negocio" onChange={handleSubmit}></input>
                                 </div>
                             </div>
                         </div>
@@ -38,14 +51,14 @@ const Suscrip = () => {
                                 <label for="" className="text-xs font-semibold px-1">Teléfono</label>
                                 <div className="flex">
                                     <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                    <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 text-gray-700" placeholder="Teléfono"></input>
+                                    <input type="text" name="telefono" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-400 outline-none focus:border-indigo-500 text-gray-700" placeholder="Teléfono" onChange={handleSubmit}></input>
                                 </div>
                             </div>
                             <div className="md:w-1/2 px-3 mb-5">
                                 <label for="" className="text-xs font-semibold px-1">Email</label>
                                 <div className="flex">
                                     <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                    <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 text-gray-700" placeholder="Email"></input>
+                                    <input type="text" name="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-400 outline-none focus:border-indigo-500 text-gray-400" placeholder="Email"></input>
                                 </div>
                             </div>
                         </div>
@@ -54,20 +67,22 @@ const Suscrip = () => {
                                 <label for="" className="text-xs font-semibold px-1">Email de gmail para conectarlo con Goolge Calendar</label>
                                 <div className="flex">
                                     <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                                    <input type="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="ejemplo@gmail.com"></input>
+                                    <input type="email" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-400 outline-none focus:border-indigo-500" placeholder="ejemplo@gmail.com"></input>
                                 </div>
                             </div>
                         </div> */}
                         <div className="flex -mx-3">
-                            <div className="w-full px-3 mb-5">
+                            <div className="w-full px-3 mb-5" type="submit" value="send message">
                                 <button className="block w-full mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">Enviar</button>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>  
         </div>
     </div>
+    </div>
+    
      );
 }
  
