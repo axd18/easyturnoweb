95b4ac47214168e3ce36d370a21fb6c177dcd2cb
adrian <adrianfernandezj@gmail.com>
2021-02-18 13:45:23 -0300

    agrego Switch en App.js para que funcione el componente del Error y cambié la imagen de los celulares en el main

--- src/App.js
+++ src/App.js
@@ -12,25 +12,27 @@ import Suscrip from './components/Suscrip';
 import Clientes from './components/Clientes';
 import Error from './components/Error';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   
 return (
     <>
       <Router>
-          <Route exact strict path="/" component={Navbar}/>
-          <Route exact strict path="/" component={Main} id="section0"/>
-          <Route exact strict path="/" component={Clientes}/>
-          <Route exact strict path="/" component={QueEs} id="section1"/>
-          <Route exact strict path="/" component={ComoFunciona} id="section2"/>
-          <Route exact strict path="/" component={Precios} id="section3"/>
-          <Route exact strict path="/" component={Faq} id="section4"/>
-          <Route exact strict path="/" component={Form} id="section5"/>
-          <Route exact strict path="/" component={Footer}/>
-          <Route exact strict path="/" component={Arrow}/>
-          <Route exact strict path="/suscripcion" component={Suscrip}/>
-          <Route component={Error}/>
+            <Route exact strict path="/" component={Navbar}/>
+            <Route exact strict path="/" component={Main} id="section0"/>
+            <Route exact strict path="/" component={Clientes}/>
+            <Route exact strict path="/" component={QueEs} id="section1"/>
+            <Route exact strict path="/" component={ComoFunciona} id="section2"/>
+            <Route exact strict path="/" component={Precios} id="section3"/>
+            <Route exact strict path="/" component={Faq} id="section4"/>
+            <Route exact strict path="/" component={Form} id="section5"/>
+            <Route exact strict path="/" component={Footer}/>
+          <Switch>
+            <Route exact strict path="/" component={Arrow}/>
+            <Route exact strict path="/suscripcion" component={Suscrip}/>
+            <Route component={Error}/>
+          </Switch>
       </Router>
     </>
   );
