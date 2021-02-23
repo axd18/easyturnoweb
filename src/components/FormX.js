d836f717285ab1244c211babca130f83b96da2fb
Adrian <adrianfernandezj@gmail.com>
2021-02-23 14:31:33 -0300

    update

--- src/components/FormX.js
+++ src/components/FormX.js
@@ -5,14 +5,14 @@ const FormX = () => {
         <section class="text-gray-600 bg-gray-400 body-font relative">
             <div class="container px-5 py-24 mx-auto">
                 <div class="flex flex-col text-center w-full mb-12">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contacto</h1>
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Envíanos un mensaje que te responderemos dentro de las 24 hs.</p>
                 </div>
                 <div class="lg:w-1/2 md:w-2/3 mx-auto">
                 <div class="flex flex-wrap -m-2">
                     <div class="p-2 w-1/2">
                     <div class="relative">
-                        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                        <label for="name" class="leading-7 text-sm text-gray-600">Nombre</label>
                         <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                     </div>
                     </div>
@@ -24,7 +24,7 @@ const FormX = () => {
                     </div>
                     <div class="p-2 w-full">
                     <div class="relative">
-                        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                        <label for="message" class="leading-7 text-sm text-gray-600">Mensaje</label>
                         <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                     </div>
                     </div>
